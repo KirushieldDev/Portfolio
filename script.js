34250cbc9a420c2974da3a7acf7931a43b261567
Kirushield <elankeethan@edu.univ-eiffel.fr>
2024-06-08 16:15:09 +0200

    Portfolio sans responsive

--- script.js
+++ script.js
@@ -15,3 +15,25 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     smoothScroll(this.getAttribute("href"));
   });
 });
+
+// Fonction pour afficher et fermer les popups
+function setupPopup(openButtonId, popupClass) {
+  document.getElementById(openButtonId).addEventListener("click", function () {
+    document.querySelector(popupClass).style.display = "flex";
+  });
+
+  document.querySelectorAll(popupClass + " .close").forEach((closeBtn) => {
+    closeBtn.addEventListener("click", function () {
+      document.querySelector(popupClass).style.display = "none";
+    });
+  });
+}
+
+// Initialisation des popups
+setupPopup("pierre-feuille-ciseau", ".popup-pierre-feuille-ciseau");
+setupPopup("juste-prix", ".popup-juste-prix");
+setupPopup("bac", ".popup-bac");
+setupPopup("calculatrice", ".popup-calculatrice");
+setupPopup("unesco", ".popup-unesco");
+setupPopup("polynome", ".popup-polynome");
+setupPopup("qix", ".popup-qix");

141eaaa1ed4ed52062896cd3a18aa57535851a65
Kirushield <elankeethan@edu.univ-eiffel.fr>
2024-06-15 11:48:27 +0200

    Ajout du projet SilverLove

--- script.js
+++ script.js
@@ -1,32 +1,34 @@
 // Fonction pour le défilement fluide
 function smoothScroll(target) {
-  const targetElement = document.querySelector(target);
-  if (targetElement) {
-    window.scrollTo({
-      top: targetElement.offsetTop,
-      behavior: "smooth",
-    });
-  }
+    const targetElement = document.querySelector(target);
+    if (targetElement) {
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: "smooth",
+        });
+    }
 }
 
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (event) {
-    event.preventDefault();
-    smoothScroll(this.getAttribute("href"));
-  });
+    anchor.addEventListener("click", function (event) {
+        event.preventDefault();
+        smoothScroll(this.getAttribute("href"));
+    });
 });
 
 // Fonction pour afficher et fermer les popups
 function setupPopup(openButtonId, popupClass) {
-  document.getElementById(openButtonId).addEventListener("click", function () {
-    document.querySelector(popupClass).style.display = "flex";
-  });
+    document
+        .getElementById(openButtonId)
+        .addEventListener("click", function () {
+            document.querySelector(popupClass).style.display = "flex";
+        });
 
-  document.querySelectorAll(popupClass + " .close").forEach((closeBtn) => {
-    closeBtn.addEventListener("click", function () {
-      document.querySelector(popupClass).style.display = "none";
+    document.querySelectorAll(popupClass + " .close").forEach((closeBtn) => {
+        closeBtn.addEventListener("click", function () {
+            document.querySelector(popupClass).style.display = "none";
+        });
     });
-  });
 }
 
 // Initialisation des popups
@@ -37,3 +39,4 @@ setupPopup("calculatrice", ".popup-calculatrice");
 setupPopup("unesco", ".popup-unesco");
 setupPopup("polynome", ".popup-polynome");
 setupPopup("qix", ".popup-qix");
+setupPopup("silverlove", ".popup-silverlove");
